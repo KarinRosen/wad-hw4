c56fe0fa2db45d82c4964459b9fc941a2b84b48f
KatariinaIngerma <katariina.ingerma@gmail.com>
2023-11-23 14:01:24 +0200

    created header component

---
+++ hw3/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>Your Website Name</h1>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">Sign up</router-link>
+      </nav>
+    </header>
+  </template>
+
+  
+<style>
+header {
+  background-color: #333;
+  padding: 10px;
+  text-align: center;
+}
+
+h1 {
+  color: #fff;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+router-link {
+  margin: 0 10px;
+  color: #fff;
+  text-decoration: none;
+}
+
+router-link:hover {
+  text-decoration: underline;
+}
+</style>
