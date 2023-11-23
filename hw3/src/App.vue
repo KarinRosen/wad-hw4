4345293e8186cdfa16dd3954711c5b04bc01341e
Your Name <karin800000@gmail.com>
2023-11-23 15:11:36 +0200

    footer

--- hw3/src/App.vue
+++ hw3/src/App.vue
@@ -1,15 +1,17 @@
 <template>
    <Header />
   <router-view/>
+  <Footer />
 </template>
 
 <script>
 
 import Header from './components/Header.vue';
-
+import Footer from './components/Footer.vue';
 export default {
   components: {
     Header,
+    Footer
   },
 };
 
---
+++ hw3/src/components/Footer.vue
@@ -0,0 +1,33 @@
+
+Vue.js on JavaScripti raamistik, mis võimaldab arendajatel ehitada kasutajaliideseid komponentide abil. Kui soovid luua Vue.js-i abil jaluse (footer) komponenti, järgneb näide selle kohta.
+
+Eeldame, et sul on juba Vue.js projekt paigaldatud. Kui ei, siis saad selle luua järgides ametlikke juhiseid: Vue.js paigaldamine.
+
+Loo uus Vue komponent, milleks võib olla näiteks "Footer.vue". Saad seda teha käsurealt või oma arenduskeskkonna kaudu.
+html
+Copy code
+<!-- Footer.vue -->
+
+<template>
+  <footer>
+    <div class="footer-content">
+      <p>&copy; 2023</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    background-color: #333;
+    color: #fff;
+    padding: 20px 0;
+    text-align: center;
+  }
+
+  .footer-content {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+</style>
+  
+    
